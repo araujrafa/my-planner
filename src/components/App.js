954909b514a098a1c112d15946cf433ce71dc5d2
araujrafa <rafael.araujo@grupofolha.com.br>
2018-10-02 18:48:00 -0300

    Adicionado firebase

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { hot } from 'react-hot-loader'
+import ToDoList from './ToDoList';
 
-const App = () => <div>Fala bixo!</div>
+const App = () => <div><ToDoList /></div>
 
 export default hot(module)(App);
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,24 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const Dotenv = require('dotenv-webpack');
+
+const webpackPlugins = [
+  new webpack.HotModuleReplacementPlugin(),
+  new MiniCssExtractPlugin({
+    filename: 'css/stylus.bundle.css'
+  }),
+  new HtmlWebPackPlugin({
+    template: "./src/templates/index.html",
+    filename: "./index.html"
+  }),
+  new Dotenv(),
+]
 
 module.exports = {
   entry: {
     app: [
+      'babel-polyfill',
       './src/index.js',
       './src/stylus/index.styl'
     ]
@@ -34,16 +48,7 @@ module.exports = {
       }
     ]
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebPackPlugin({
-      template: "./src/templates/index.html",
-      filename: "./index.html"
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'css/stylus.bundle.css'
-    }),
-  ],
+  plugins: webpackPlugins,
   output: {
     filename: 'js/app.bundle.js',
     path: path.resolve(__dirname, 'public')
